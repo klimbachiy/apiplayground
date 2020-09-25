e13e34e1d87483a7f7e0664be0b46f3c0e078269
klimbachiy <klimbachiy@wiley.com>
2020-09-25 19:22:33 +0530

    Updates

---
+++ precache-manifest.2af76d45a10e93f43a93dd9375995cc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df3acdeaf48ab84c5ace830dd355c98f",
+    "url": "/apiplayground/index.html"
+  },
+  {
+    "revision": "73269469a142641ee0f3",
+    "url": "/apiplayground/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fc89bfb9aca56e77fa4c",
+    "url": "/apiplayground/static/js/2.7a8b8b7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/apiplayground/static/js/2.7a8b8b7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73269469a142641ee0f3",
+    "url": "/apiplayground/static/js/main.e8e3feaa.chunk.js"
+  },
+  {
+    "revision": "3b8240ab8af773cdb45b",
+    "url": "/apiplayground/static/js/runtime-main.e6250537.js"
+  },
+  {
+    "revision": "26f042ceef503d7a647d31ffad88bddc",
+    "url": "/apiplayground/static/media/covid.26f042ce.jpg"
+  }
+]);
